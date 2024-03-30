24e4b90121b01833d2cc25e5d3c97a2ae2ee55f3
nnaemyx <edehjohnpaul@gmail.com>
2024-03-30 16:55:24 -0400

    new

--- models/meetupModel.js
+++ models/meetupModel.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const meetupSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
+  images: { type: String, required: true },
+  slug: { type: String, required: true },
 });
 
-const Meetup = mongoose.model('Meetup', meetupSchema);
+const Meetup = mongoose.model("Meetup", meetupSchema);
 
 module.exports = Meetup;
